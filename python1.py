5d1ea5d2f1223dde0b78d3cfea14d86f2f733551
Ashish Kumar Patel <ashishkumar7773333gmail.com>
2025-09-26 21:04:05 +0530

    Writing first python coding

---
+++ python1.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("hello Ashish")
+print("my age is 21")
+#taking variable
+name = "ashish"
+print(name)
+age = 21
+price = 25.43
+print (age)
+print(price)
+#variable type
+print(type(price))
+print(type(name))
+print(type(age))
+
+
