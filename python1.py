4973582b90f5e56162db44cc447f4e694be7db62
Ashish Kumar Patel <ashishkumar7773333gmail.com>
2025-09-26 21:27:42 +0530

    add some comments

--- python1.py
+++ python1.py
@@ -1,16 +1,18 @@
-print("hello world")
+print("hello world") # simple code
 print("hello Ashish")
 print("my age is 21")
-#taking variable
-name = "ashish"
+
+
+name = "ashish" # Taking varible
 print(name)
 age = 21
 price = 25.43
 print (age)
 print(price)
-#variable type
-print(type(price))
-print(type(name))
+
+
+print(type(name)) # To check variable type
+print(type(price)) 
 print(type(age))
 
 
